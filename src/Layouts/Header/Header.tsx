53a9cabcbf2208c8aaf6ea53ceff44234e169007
Ali.Cheraghi <alicheraghi148@gmail.com>
2023-05-10 03:21:56 +0330

    sticky header

--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <nav className='flex items-center justify-between flex-wrap bg-blue-950 py-3 px-6'>
+    <nav className='flex items-center justify-between flex-wrap bg-blue-950 py-3.5 px-6 sticky top-0 z-50'>
       <div className='flex items-center flex-shrink-0 text-white mr-6'>
         <span className='font-semibold text-xl tracking-tight'>
           Exchange App
